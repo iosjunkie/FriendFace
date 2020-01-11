bfa456da82c1cf6e1f1a5f9e0e9b36fa4c977a61
Jules Lee <jackconsolidated@gmail.com>
2020-01-11 09:57:55 +0800

    Almost done.. Friends are missing

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2020 Jules Lee. All rights reserved.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
     @State private var users = User.fetch()
+    
     var body: some View {
         NavigationView {
             List(users) { user in
@@ -18,7 +21,37 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle("Users")
+            .navigationBarItems(leading: Button("DeleteAll") {
+                    self.deleteAll()
+                }, trailing: Button("Fetch") {
+                    self.fetch()
+                }
+            )
         }
+    }
+    
+    func deleteAll() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>()
+
+        fetchRequest.entity = CDUser.entity()
+        fetchRequest.includesPropertyValues = false
+
+        do
+        {
+            let results = try moc.fetch(fetchRequest)
+            for managedObject in results
+            {
+                if let object = managedObject as? NSManagedObject {
+                    moc.delete(object)
+                }
+            }
+        } catch let error as NSError {
+            print("Delete All Error : \(error) ")
+        }
+    }
+    
+    func fetch() {
+        users = User.fetch()
     }
 }
 
--- FriendFace/User.swift
+++ FriendFace/User.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2020 Jules Lee. All rights reserved.
 //
 
-import Foundation
+import CoreData
+import SwiftUI
 
 struct User: Codable, Identifiable {
     var id: UUID
@@ -16,35 +17,96 @@ struct User: Codable, Identifiable {
     var friends: [Friend]
     
     static func fetch() -> [User] {
-        let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")
-        let request = URLRequest(url: url!)
         
-        var users: [User] = [User]()
+        let cdUsers = User.fetchCoreData()
         
-        let semaphore = DispatchSemaphore(value: 0)
+        var users: [User] = [User]() {
+            didSet {
+                if cdUsers.count == 0 {
+                    DispatchQueue.main.async {
+                        User.saveToCoreData(users: users)
+                    }
+                }
+            }
+        }
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                fatalError("Network error: " + error.localizedDescription)
-            }
-            guard let response = response as? HTTPURLResponse else {
-                fatalError("Not a HTTP response")
-            }
-            guard response.statusCode >= 200, response.statusCode < 300 else {
-                fatalError("Invalid HTTP status code")
-            }
-            guard let data = data else {
-                fatalError("No HTTP data")
-            }
+        print(cdUsers.count)
+        if cdUsers.count > 0 {
+            users = cdUsers
+            print("from cdusers")
+        } else {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")
+            let request = URLRequest(url: url!)
             
-            if let decodedUsers = try? JSONDecoder().decode([User].self, from: data) {
-                users = decodedUsers
-                semaphore.signal()
-            }
-        }.resume()
-        
-        _ = semaphore.wait(wallTimeout: .distantFuture)
+            let semaphore = DispatchSemaphore(value: 0)
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    fatalError("Network error: " + error.localizedDescription)
+                }
+                guard let response = response as? HTTPURLResponse else {
+                    fatalError("Not a HTTP response")
+                }
+                guard response.statusCode >= 200, response.statusCode < 300 else {
+                    fatalError("Invalid HTTP status code")
+                }
+                guard let data = data else {
+                    fatalError("No HTTP data")
+                }
+                
+                if let decodedUsers = try? JSONDecoder().decode([User].self, from: data) {
+                    users = decodedUsers
+                    print("from users")
+                    
+                    semaphore.signal()
+                }
+            }.resume()
+            
+            _ = semaphore.wait(wallTimeout: .distantFuture)
+        }
         
         return users
+    }
+    
+    static func fetchCoreData() -> [User] {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<CDUser>(entityName: "CDUser")
+        var users = [User]()
+        
+        moc.performAndWait {
+            let results: [CDUser] = try! request.execute()
+            for user in results {
+                var friends = [Friend]()
+                for friend in user.friends! {
+                    let friend = friend as! Friend
+                    friends.append(Friend(id: friend.id, name: friend.name))
+                }
+                
+                users.append(User(id: user.id!, name: user.name!, age: user.age, company: user.company!, friends: friends))
+            }
+        }
+        return users
+    }
+    
+    static func saveToCoreData(users: [User]) {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        moc.performAndWait {
+            users.forEach { user in
+                let cdUser = CDUser(context: moc)
+                cdUser.id = user.id
+                cdUser.name = user.name
+                cdUser.age = Int16(user.age)
+                cdUser.company = user.company
+                var friends = [Friend]()
+                user.friends.forEach { friend in
+                    friends.append(friend)
+                }
+                cdUser.friends?.addingObjects(from: friends)
+            }
+        }
+        
+        if moc.hasChanges {
+            try? moc.save()
+        }
     }
 }
